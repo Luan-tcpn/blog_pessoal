236d7c649d84ed5bd22084c44dc2c125ffd7e120
Luan-tcpn <luanoliveiraa200@gmail.com>
2025-01-10 13:45:43 -0300

    Passos 5 e 6 do indice

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,62 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+
+
+public class Postagem {
+	
+	
+	@Id // chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // mais caracteres que int
+	
+	@NotBlank(message = "Esse campo é obrigatório") // not null
+	@Size(min = 5, max = 100, message = "Digite no minimo 5 no maximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "Esse campo é obrigatório") // not null
+	@Size(min = 10, max = 1000, message = "Digite no minimo 10 no maximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+}
