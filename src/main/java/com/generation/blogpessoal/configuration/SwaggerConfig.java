ca26ade0df160d3def65cab5f5c03c02350f1bbe
Luan-tcpn <luanoliveiraa200@gmail.com>
2025-01-30 02:44:28 -0300

    deploy

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -23,15 +23,15 @@ public class SwaggerConfig {
                 .description("Projeto Blog Pessoal - Generation Brasil")
                 .version("v0.0.1")
                 .license(new License()
-                    .name("Generation Brasil")
+                    .name("Luan Oliveira")
                     .url("https://brazil.generation.org/"))
                 .contact(new Contact()
-                    .name("Generation Brasil")
-                    .url("https://github.com/conteudoGeneration")
-                    .email("conteudogeneration@generation.org")))
+                    .name("Luan Oliveira")
+                    .url("https://github.com/luan-tcpn")
+                    .email("luanf42@gmail.com")))
             .externalDocs(new ExternalDocumentation()
                 .description("Github")
-                .url("https://github.com/conteudoGeneration/"));
+                .url("https://github.com/luan-tcpn/"));
     }
 
 
